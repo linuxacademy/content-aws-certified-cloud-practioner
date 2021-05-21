546b0cd0dca53e1a711a6fa505ea602332296af8
Kesha Williams <kesha.williams@acloud.guru>
2021-05-21 13:49:55 -0400

    updated lab code

--- labs/create-a-lambda-function-with-the-aws-management-console/lambda_function.py
+++ labs/create-a-lambda-function-with-the-aws-management-console/lambda_function.py
@@ -1,5 +1,9 @@
 def lambda_handler(event, context):
     message = 'Hello {} {}! Keep being awesome!'.format(event['first_name'], event['last_name'])  
+
+    #print to CloudWatch logs
+    print(message)
+
     return { 
         'message' : message
-    }
+    }   
